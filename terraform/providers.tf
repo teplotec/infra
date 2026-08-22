91695e6fc50a2873771fc1ed10af37830b038deb
Serhii Ponomarov <sergii.ponomarov@gmail.com>
2026-08-22 11:42:57 +0300

    Pin Terraform providers

---
+++ terraform/providers.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = "~> 1.15.0"
+
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "5.23.0"
+    }
+
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = "1.68.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.7"
+    }
+  }
+}
+
+provider "cloudflare" {}
+provider "hcloud" {}
